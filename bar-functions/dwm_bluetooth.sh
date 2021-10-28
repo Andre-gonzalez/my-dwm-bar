0017b892b80f7e686246ab21ef9f208b836bcfd4
André Gonzalez <lopescg@gmail.com>
2021-10-28 11:57:42 -0300

    added a bluetooth battery indicator

--- bar-functions/dwm_bluetooth.sh
+++ bar-functions/dwm_bluetooth.sh
@@ -2,10 +2,11 @@
 dwm_bluetooth () {
 		xm3=$(bluetoothctl info CC:98:8B:C1:5C:78 | sed -n '9p')
 		redmi=$(bluetoothctl info 1C:52:16:D1:A1:9B | sed -n '9p')
+		BATTERY=$(bluetooth-headset-battery-level $(bluetoothctl info | awk '/^Device/ {print $2}'))
 		if [ "$xm3" = "	Connected: yes" ]; then
-						printf " XM3"
+						printf " XM3 %s" ${BATTERY: -3}
 		elif [ "$redmi" = "	Connected: yes" ]; then
-						printf " redmi"
+						printf " redmi %s" ${BATTERY: -3} 
 		else	
 						printf ""
 		fi
