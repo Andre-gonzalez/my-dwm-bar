e841573251668febf15d473d76f11c1860c32908
André Gonzalez <lopescg@gmail.com>
2021-10-29 12:18:31 -0300

    added battery %

--- bar-functions/dwm_bluetooth.sh
+++ bar-functions/dwm_bluetooth.sh
@@ -2,11 +2,12 @@
 dwm_bluetooth () {
 		xm3=$(bluetoothctl info CC:98:8B:C1:5C:78 | sed -n '9p')
 		redmi=$(bluetoothctl info 1C:52:16:D1:A1:9B | sed -n '9p')
-		BATTERY=$(bluetooth-headset-battery-level $(bluetoothctl info | awk '/^Device/ {print $2}'))
+		BATTERY=$(bluetooth-headset-battery-level $(bluetoothctl info | awk '/^Device/ {print $2}')|
+tail -c 5)
 		if [ "$xm3" = "	Connected: yes" ]; then
-						printf " XM3 %s" ${BATTERY: -3}
+						printf " XM3%s" "$BATTERY"
 		elif [ "$redmi" = "	Connected: yes" ]; then
-						printf " redmi %s" ${BATTERY: -3} 
+						printf " redmi%s" "$BATTERY"
 		else	
 						printf ""
 		fi
